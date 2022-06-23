5d31a1f1ac0a5aaec5cb273602c3ac10b8869fbc
cibs84 <d.tilesi@gmail.com>
2022-06-23 17:52:10 +0200

    bonus completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var app = new Vue ({
     el: '#root',
     data: {
-        message: 'Hello World!'
+        message: 'Hello World!',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png'
     }
 })
